b50dd3534bc8fd8010eca6fa633b3edffe5766eb
RAMIRO <ragahe@gmail.com>
2023-06-19 23:11:43 -0300

    combate 1v1 v3

--- Combate.cs
+++ Combate.cs
@@ -16,7 +16,35 @@ public class Combates{
     public Personaje Combate(Personaje p1, Personaje p2){
         var valor = new Random();
         float daño;
-        Personaje auxp1 = p1, auxp2 = p2;
+        Personaje auxp1 = new Personaje(), auxp2 = new Personaje();
+        auxp1.Nombre = p1.Nombre;
+        auxp1.Apodo = p1.Apodo;
+        auxp1.Edad = p1.Edad;
+        auxp1.Nivel = p1.Nivel;
+        auxp1.Especial = p1.Especial;
+        auxp1.FechaDeNacimiento = p1.FechaDeNacimiento;
+        auxp1.Tipo = p1.Tipo;
+        auxp1.Salud = p1.Salud;
+        auxp1.Fuerza = p1.Fuerza;
+        auxp1.Destreza = p1.Destreza;
+        auxp1.Velocidad = p1.Velocidad;
+        auxp1.Energia = p1.Energia;
+        auxp1.Armadura = p1.Armadura;
+
+        auxp2.Nombre = p2.Nombre;
+        auxp2.Apodo = p2.Apodo;
+        auxp2.Edad = p2.Edad;
+        auxp2.Nivel = p2.Nivel;
+        auxp2.Especial = p2.Especial;
+        auxp2.FechaDeNacimiento = p2.FechaDeNacimiento;
+        auxp2.Tipo = p2.Tipo;
+        auxp2.Salud = p2.Salud;
+        auxp2.Fuerza = p2.Fuerza;
+        auxp2.Destreza = p2.Destreza;
+        auxp2.Velocidad = p2.Velocidad;
+        auxp2.Energia = p2.Energia;
+        auxp2.Armadura = p2.Armadura;
+
         int i = 1;
         while(p1.Salud>0 && p2.Salud>0){
             System.Console.WriteLine($"************************RONDA "+ i +"************************");
@@ -31,8 +59,13 @@ public class Combates{
             System.Console.WriteLine("-------------------------------------------------");
             if(p1.Velocidad>p2.Velocidad){
                 System.Console.WriteLine("TURNO DE " + p1.Nombre);
-                switch(valor.Next(1,6)){
+                switch(valor.Next(1,11)){
                     case 1://Ataque normal
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
                         if(p1.Energia>=2){
                             System.Console.WriteLine(p1.Nombre + ", hizo un ataque normal");
                             daño = DañoProvocado(p1,(p2.Armadura*p2.Velocidad));
@@ -42,14 +75,14 @@ public class Combates{
                             System.Console.WriteLine(p1.Nombre + ", no tiene energias para su ataque normal");
                         }
                         break;
-                    case 2://defensa
-                    case 3:
-                    case 4:
+                    case 7://defensa
+                    case 8:
+                    case 9:
                         p1.Armadura += 1;
                         p1.Energia +=2;
                         System.Console.WriteLine(p1.Nombre + ", aumentó su defensa");
                         break;
-                    case 5://Habilidad especial
+                    case 10://Habilidad especial
                         if(p1.Energia>=4){
                             System.Console.WriteLine(p1.Nombre + ", usó su habilidad especial");
                             if(p1.Especial == "Escudo protector" || p1.Especial == "Flecha anestésica" || p1.Especial == "Frío envolvente" || p1.Especial == "Vendas"){
@@ -67,8 +100,13 @@ public class Combates{
                         break;
                 }
                 if(p2.Salud>0){
-                    switch(valor.Next(1,6)){
+                    switch(valor.Next(1,11)){
                         case 1://Ataque normal
+                        case 2:
+                        case 3:
+                        case 4:
+                        case 5:
+                        case 6:
                             if(p2.Energia>=2){
                                 System.Console.WriteLine(p2.Nombre + ", hizo un ataque normal");
                                 daño = DañoProvocado(p2,(p1.Armadura*p1.Velocidad));
@@ -78,14 +116,14 @@ public class Combates{
                                 System.Console.WriteLine(p2.Nombre + ", no tiene energias para su ataque normal");
                             }
                             break;
-                        case 2://defensa
-                        case 3:
-                        case 4:
+                        case 7://defensa
+                        case 8:
+                        case 9:
                             p2.Armadura += 1;
                             p2.Energia +=2;
                             System.Console.WriteLine(p2.Nombre + ", aumentó su defensa");
                             break;
-                        case 5://Habilidad especial
+                        case 10://Habilidad especial
                             if(p2.Energia>=4){
                                 System.Console.WriteLine(p2.Nombre + ", usó su habilidad especial");
                                 if(p2.Especial == "Escudo protector" || p2.Especial == "Flecha anestésica" || p2.Especial == "Frío envolvente" || p2.Especial == "Vendas"){
@@ -104,8 +142,13 @@ public class Combates{
                     }
                 }
             }else{
-                switch(valor.Next(1,6)){
+                switch(valor.Next(1,11)){
                     case 1://Ataque normal
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
                         if(p2.Energia>=2){
                             System.Console.WriteLine(p2.Nombre + ", hizo un ataque normal");
                             daño = DañoProvocado(p2,(p1.Armadura*p1.Velocidad));
@@ -115,14 +158,14 @@ public class Combates{
                             System.Console.WriteLine(p2.Nombre + ", no tiene energias para su ataque normal");
                         }
                         break;
-                    case 2://defensa
-                    case 3:
-                    case 4:
+                    case 7://defensa
+                    case 8:
+                    case 9:
                         p2.Armadura += 1;
                         p2.Energia +=2;
                         System.Console.WriteLine(p2.Nombre + ", aumentó su defensa");
                         break;
-                    case 5://Habilidad especial
+                    case 10://Habilidad especial
                         if(p2.Energia>=4){
                             System.Console.WriteLine(p2.Nombre + ", usó su habilidad especial");
                             if(p2.Especial == "Escudo protector" || p2.Especial == "Flecha anestésica" || p2.Especial == "Frío envolvente" || p2.Especial == "Vendas"){
@@ -140,8 +183,13 @@ public class Combates{
                         break;
                 }
                 if(p1.Salud>0){
-                    switch(valor.Next(1,6)){
+                    switch(valor.Next(1,11)){
                         case 1://Ataque normal
+                        case 2:
+                        case 3:
+                        case 4:
+                        case 5:
+                        case 6:
                             if(p1.Energia>=2){
                                 System.Console.WriteLine(p1.Nombre + ", hizo un ataque normal");
                                 daño = DañoProvocado(p1,(p2.Armadura*p2.Velocidad));
@@ -151,14 +199,14 @@ public class Combates{
                                 System.Console.WriteLine(p1.Nombre + ", no tiene energias para su ataque normal");
                             }
                             break;
-                        case 2://defensa
-                        case 3:
-                        case 4:
+                        case 7://defensa
+                        case 8:
+                        case 9:
                             p1.Armadura += 1;
                             p1.Energia +=2;
                             System.Console.WriteLine(p1.Nombre + ", aumentó su defensa");
                             break;
-                        case 5://Habilidad especial
+                        case 10://Habilidad especial
                             if(p1.Energia>=4){
                                 System.Console.WriteLine(p1.Nombre + ", usó su habilidad especial");
                                 if(p1.Especial == "Escudo protector" || p1.Especial == "Flecha anestésica" || p1.Especial == "Frío envolvente" || p1.Especial == "Vendas"){
@@ -239,7 +287,7 @@ public class Combates{
         }
         ataque = p1.Fuerza * p1.Destreza * p1.Nivel;
 
-        daño = ((ataque * efe)- defensa)/500;
+        daño = ((ataque * efe)- defensa)/250;
 
         return daño;
     }
